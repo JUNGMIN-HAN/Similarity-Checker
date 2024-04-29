cb4da006d06596c3078237267ba74de89be31fbd
jungmin <gmlsajfl93@gmail.com>
2024-04-29 14:51:25 +0900

    isTwiceMoreLengthDifference 메소드 명 변경

--- src/main/java/Similarity_Checker.java
+++ src/main/java/Similarity_Checker.java
@@ -12,7 +12,7 @@ public class Similarity_Checker {
         int len2 = str2.length();
         int gap, denominator;
 
-        if (isTwiceMoreLength(len1, len2)) return 0;
+        if (isTwiceMoreLengthDifference(len1, len2)) return 0;
 
         if (len1 > len2) {
             gap = len1 - len2;
@@ -25,7 +25,7 @@ public class Similarity_Checker {
         return (int) ((1 - (double) gap / denominator) * 60);
     }
 
-    private boolean isTwiceMoreLength(int len1, int len2) {
+    private boolean isTwiceMoreLengthDifference(int len1, int len2) {
         return len1 >= 2 * len2 || len2 >= 2 * len1;
     }
 }
