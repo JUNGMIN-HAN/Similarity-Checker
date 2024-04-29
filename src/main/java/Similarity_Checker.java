bca112dce990d73752bab7d2642b41600120dc4c
jungmin <gmlsajfl93@gmail.com>
2024-04-29 14:44:13 +0900

    getlengthScore 기능 및 TEST 추가

--- src/main/java/Similarity_Checker.java
+++ src/main/java/Similarity_Checker.java
@@ -1,2 +1,31 @@
 public class Similarity_Checker {
+    String str1;
+    String str2;
+
+    public Similarity_Checker(String str1, String str2) {
+        this.str1 = str1;
+        this.str2 = str2;
+    }
+
+    public int getlengthScore() {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        int gap, denominator;
+
+        if (isTwiceMoreLength(len1, len2)) return 0;
+
+        if (len1 > len2) {
+            gap = len1 - len2;
+            denominator = len2;
+        } else {
+            gap = len2 - len1;
+            denominator = len1;
+        }
+
+        return (int) ((1 - (double) gap / denominator) * 60);
+    }
+
+    private boolean isTwiceMoreLength(int len1, int len2) {
+        return len1 >= 2 * len2 || len2 >= 2 * len1;
+    }
 }
